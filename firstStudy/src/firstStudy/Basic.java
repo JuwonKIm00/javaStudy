b5555a6dcd7e040f24f9f60d9407df445bc51bb8
james_kjw <103508921+JuwonKIm00@users.noreply.github.com>
2022-04-20 07:41:07 +0900

    init commit
    first github upload

---
+++ firstStudy/src/firstStudy/Basic.java
@@ -0,0 +1,11 @@
+package firstStudy;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		System.out.println("myFirstJavaStudy!");
+	}
+
+}
