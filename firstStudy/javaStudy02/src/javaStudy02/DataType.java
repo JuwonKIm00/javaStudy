6d7a1e8e660fd3687d017294bbce0ee2bb0b2a67
james_kjw <103508921+JuwonKIm00@users.noreply.github.com>
2022-04-20 19:12:59 +0900

    init commit
    second java study

---
+++ firstStudy/javaStudy02/src/javaStudy02/DataType.java
@@ -0,0 +1,23 @@
+package javaStudy02;
+
+public class DataType {
+	
+	public static void main(String[] args) {
+		
+		/* 자료형			형식지정자
+		 * int(정수)		%d
+		 * double(실수) 	%f
+		 * char(문자)		%c
+		 * String(문자열)	%s
+		 */
+		
+		/* 제어문자
+		 * \t
+		 * \n
+		 * \"
+		 * \\
+		 */
+		
+		
+	}
+}
---
+++ firstStudy/javaStudy02/src/javaStudy02/PrintMethod.java
@@ -0,0 +1,23 @@
+package javaStudy02;
+
+public class PrintMethod {
+
+	public static void main(String[] args) {
+		
+		//쌍 따옴표는 문자열(String) 을 알려주기 위해 사용한다
+		System.out.println("반갑습니다");
+		System.out.println("안녕");
+		
+		//작은 따옴표는 문자(char)을 알려주기 위해 사용한다 
+		System.out.println('a');
+		
+		//println 은 줄 바꿈까지 해준다
+		//print 는 줄바꿈을 포함하지 않는다 
+		//printf는 형식을 지정하여 출력한다 
+		
+		System.out.print('a');
+		
+		System.out.printf("%d 는 숫자 1이다",1);
+	}
+
+}
